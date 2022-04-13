8f47dba76ba7bd6f438851f2f35f5f0a5d654615
Matthew Williams <mattwilliamsdev@gmail.com>
2022-04-13 14:32:26 +0100

    Added the me query to the resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,3 +2,16 @@ const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
+const resolvers = {
+  Query: {
+    me: async (_, _, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id });
+        return userData;
+      }
+      throw new AuthenticationError("Please log in");
+    },
+  }
+}
+
+module.exports = resolvers;
